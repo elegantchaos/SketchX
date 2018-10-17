e6168028c7c61bf3bf7d809cd3882fc3ddda647a
Sam Deane <sam@elegantchaos.com>
2018-10-17 10:09:53 +0100

    added hello world

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SketchX",
+    products: [
+        .executable(name: "sketchx", target: "SketchX")
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
