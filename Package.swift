5a765c2101685260c3fb26aff5c7628efc222077
Sam Deane <sam@elegantchaos.com>
2018-10-17 11:52:08 +0100

    Updated Arguments version.

--- Package.swift
+++ Package.swift
@@ -9,15 +9,14 @@ let package = Package(
         .executable(name: "sketchx", targets: ["SketchX"])
     ],
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/Arguments", from: "1.0.0"),
-        .package(url: "https://github.com/elegantchaos/Runner", from: "1.0.0"),
+        .package(url: "https://github.com/elegantchaos/Runner", from: "1.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SketchX",
-            dependencies: ["Arguments", "Runner"]),
+            dependencies: ["Runner"]),
         .testTarget(
             name: "SketchXTests",
             dependencies: ["SketchX"]),
