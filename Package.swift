d5c2ace540ab133128acd08004605eea8058571e
Sam Deane <sam@elegantchaos.com>
2018-10-17 10:28:20 +0100

    Added Runner

--- Package.swift
+++ Package.swift
@@ -9,15 +9,15 @@ let package = Package(
         .executable(name: "sketchx", targets: ["SketchX"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/elegantchaos/Arguments", from: "1.0.0"),
+        .package(url: "https://github.com/elegantchaos/Runner", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SketchX",
-            dependencies: []),
+            dependencies: ["Arguments", "Runner"]),
         .testTarget(
             name: "SketchXTests",
             dependencies: ["SketchX"]),
