c3d781ce9cd2f0b36f6235c2867f57a36f8b2fe2
Sam Deane <sam@elegantchaos.com>
2019-06-17 12:57:14 +0100

    more dependency updates

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
         .executable(name: "sketchx", targets: ["SketchX"])
     ],
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/Runner.git", from: "1.0.1"),
-        .package(url: "https://github.com/elegantchaos/CommandShell.git", from: "1.0.2"),
+        .package(url: "https://github.com/elegantchaos/Runner.git", from: "1.0.2"),
+        .package(url: "https://github.com/elegantchaos/CommandShell.git", from: "1.0.5"),
     ],
     targets: [
         .target(
