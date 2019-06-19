ed29058712f41c531f5e7b6c1af605a88f315a8b
Sam Deane <sam@elegantchaos.com>
2019-06-19 11:41:45 +0100

    updated CommandShell

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Runner.git", from: "1.0.2"),
-        .package(url: "https://github.com/elegantchaos/CommandShell.git", from: "1.0.5"),
+        .package(url: "https://github.com/elegantchaos/CommandShell.git", from: "1.1.0"),
     ],
     targets: [
         .target(
